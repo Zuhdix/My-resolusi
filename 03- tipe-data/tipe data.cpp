db3969765ea7f4274dde1f111db1c7115191fbf3
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-01-19 22:47:46 +0700

    Update tipe data.cpp

--- 03- tipe-data/tipe data.cpp
+++ 03- tipe-data/tipe data.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 
 	// bilangan bulat
-	unsigned int a = 5; // 32-bit
+	unsigned int a = 5; // 32-bit >> setiap tipe data memiliki max limit
 	long b = 6;
 	short c = 7;
 
@@ -21,7 +21,7 @@ int main(){
 
 	cout << a << endl;
 	cout << sizeof(a) << " byte" << endl;
-	cout << numeric_limits<unsigned int>::max() << endl;
+	cout << numeric_limits<unsigned int>::max() << endl; // ini adalah cara untuk melihat max dari tipe data tertentu 
 	cout << numeric_limits<unsigned int>::min() << endl;
 	cin.get();
 	return 0;
