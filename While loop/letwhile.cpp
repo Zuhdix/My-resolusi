917cb8a9b6276caaa3db2f764c1787db266f151a
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-02-15 00:27:28 +0700

    Update letwhile.cpp

--- While loop/letwhile.cpp
+++ While loop/letwhile.cpp
@@ -8,9 +8,9 @@ int main()
 	int a = 5;
 	
 	while(a <= 10){
-		cout << "hore ";
-		cout << a << endl;
-		a += 1;
+		cout << "hore "; // jika while terus true maka akan looping terus menerus
+		cout << a << endl;  // baris di {} disebut aksi looping
+		a += 1; 	// karena ada nilai 1 setiap looping maka akan terjadi false
 	}
 
 	cout << "selesai" << endl;
