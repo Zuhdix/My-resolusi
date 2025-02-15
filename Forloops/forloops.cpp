48abc2434b8716b89acf4e94fd33fe02855d4ad1
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-02-15 23:06:56 +0700

    Update forloops.cpp

--- Forloops/forloops.cpp
+++ Forloops/forloops.cpp
@@ -7,24 +7,24 @@ int main()
 	
 	cout << "Loop 1 \n"; 
 	for(int i = 1; i <= 10; i++){
-		cout << i << endl; 
+		cout << i << endl; 	// for loop increment
 	}
 
 	cout << "\n Loop2 \n";
 	for(int i = 1; i <= 10; i += 2 ){
-		cout << i << endl; 
+		cout << i << endl; 	// for loop operator assignment
 	}
 
 	cout << "\n Loop3 \n";
 	for(int i = 1; i >= -10; i-- ){
-		cout << i << endl; 
+		cout << i << endl; 	// for loop decrement
 	}
 
 	cout << "\n Loop4 \n";
 
 	int total = 0;
 	for(int i = 1; i <= 10; i++, total += i){
-		cout << i << " || " << total << endl; 
+		cout << i << " || " << total << endl; // manipulasi for loop dengan tambahan int atau campuran ( compound )
 	}
 
 
