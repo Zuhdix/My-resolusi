7d8d6f127bf893e8e37adc9e4768817573ecb49e
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-06-19 22:29:24 +0700

    Create 2.2 parameter.cpp
    Dalam program ini, variabel num pertama kali diinisialisasi dengan nilai yang dimasukkan oleh pengguna. Kemudian, fungsi printDouble dipanggil, dan nilai argumen num disalin ke dalam parameter nilai fungsi printDouble. Fungsi printDouble kemudian menggunakan nilai nilai parameter.

---
+++ LearnCpp/2 Series Dua/2.2 parameter.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+int getValueFromUser()
+{
+	std::cout << "Enter an integer: ";
+	int input{};
+	std::cin >> input;
+
+	return input;
+}
+
+void printDouble(int value)
+{
+	std::cout << value << " Double is " << value * 2 << '\n';
+}
+
+int main()
+{
+	int num{ getValueFromUser() }; // meminta user memasukkan nilai
+
+	printDouble(num); // nilai disalin ke num trus dijalankan ke fungsi printDouble
+
+	return 0;
+
+}
