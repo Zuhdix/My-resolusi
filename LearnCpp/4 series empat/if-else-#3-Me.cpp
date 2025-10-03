bea4ad24c6bf0932f4459e0a296a86127c7092cf
Aqiel Maulidan Zuhdi <maulidanzuhdi@gmail.com>
2025-10-03 22:20:09 +0700

    iya

---
+++ LearnCpp/4 series empat/if-else-#3-Me.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+bool isAllowedToTakeFunRide(double height)
+{
+	if (height >= 140.0)
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	std::cout << "How tall are you (cm)\n";
+
+	int x{};
+	std::cin >> x;
+	
+	if (isAllowedToTakeFunRide(x))
+		std::cout << " Have fun\n";
+	else
+		std::cout << " sorry, you're to short.\n";
+
+	return 0;
+}
